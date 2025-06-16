8c033b51c68e26de98f33b0ab38dd8c4a6f3b693
Max <66534050+0DevMax@users.noreply.github.com>
2025-06-16 10:47:55 -0300

    Typo

--- formatador_hyper.py
+++ formatador_hyper.py
@@ -83,7 +83,7 @@ if arquivo_principal is not None:
     
     mapeamento_bancos = {
         2: 'MeuCash',
-        243: 'Master',,
+        243: 'Master',
         318: 'BMG',
         389: 'Mercantil',
         422: 'Safra',
