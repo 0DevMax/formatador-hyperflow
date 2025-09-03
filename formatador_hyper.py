f1520c66eaeb62f32106aab56a40b4a7452d1e34
Max <66534050+0DevMax@users.noreply.github.com>
2025-09-03 11:23:55 -0300

    File uploader
    accept_multiple_files=True

--- formatador_hyper.py
+++ formatador_hyper.py
@@ -45,7 +45,7 @@ st.sidebar.write("------")
 
 campanha = st.sidebar.selectbox("Tipo da Campanha:", ['Novo', 'Benefício', 'Cartão'])
 
-arquivo_principal = st.file_uploader("Escolha o arquivo", type=['csv'])
+arquivo_principal = st.file_uploader("Escolha o arquivo", type=['csv'], accept_multiple_files=True)
 
 if arquivo_principal is not None:
     if campanha == 'Novo':
